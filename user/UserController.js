d27e8c412d9ff681c14c780e5d6e42de9aa7d4aa
ARITRA BASU <aritrabasu104@gmail.com>
2019-10-23 17:28:25 +0530

    moved db access to service layer

--- user/UserController.js
+++ user/UserController.js
@@ -5,85 +5,36 @@ const mongoose = require('mongoose');
 const ObjectId = mongoose.Types.ObjectId;
 router.use(bodyParser.urlencoded({ extended: true }));
 router.use(bodyParser.json());
-const User = require('./models/User');
 const Player = require('./models/Player');
 const Match = require('./models/Match');
 const PlayerStat = require('./models/PlayerStat');
+const UserService = require('./UserService');
 
 // CREATES A NEW USER
 router.post('/createPlayer', (req, res) => {
-    Player.create({
-        playerName: req.body.name,
-    }).then(player => res.status(200).send(player))
-        .catch(err => res.status(500).send(err.message + " : There was a problem adding the information to the database."));
-});
-router.post('/createMatch', (req, res) => {
-    Match.create({
-        matchName: req.body.name,
-    }).then(match => res.status(200).send(match))
+    UserService.createPlayer(req.body.name).then(player => res.status(200).send(player))
         .catch(err => res.status(500).send(err.message + " : There was a problem adding the information to the database."));
 });
 
+// CREATES A NEW MATCH
+router.post('/createMatch', (req, res) => {
+    UserService.createMatch(req.body.name).then(match => res.status(200).send(match))
+        .catch(err => res.status(500).send(err.message + " : There was a problem adding the information to the database."));
+});
+
+// CREATES A NEW STAT
 router.post('/playerStat', (req, res) => {
-    //Player.findById(req.body.player).then(player => {
     if (!req.body.player || req.body.player.trim().length === 0) return res.status(404).send("No user found.");
     else if (!req.body.match || req.body.match.trim().length === 0) return res.status(404).send("No Match found.");
-    Match.find({ matchName: req.body.match }, (err, matches) => {
-        if (err) return res.status(500).send(err.message + " : There was a problem finding the match.");
-        if (matches.length === 0) return res.status(404).send("Invalid match name provided.");
-        const currentMatch = matches[0];
-        PlayerStat.find({
-            player: ObjectId(req.body.player),
-            match: currentMatch
-        }, (err, playerStats) => {
-            const playerStatNew = {
-                player: ObjectId(req.body.player),
-                match: currentMatch,
-                kills: req.body.kills,
-                score: req.body.score,
-                statTime: new Date().getTime()
-            };
-            if (err) return res.status(500).send(err.message + " : There was a problem finding the player stats.");
-            if (playerStats.length === 0) {
-                createPlayerAndUpdateRanks(playerStatNew, res, currentMatch);
-            } else {
-                const playerStatOld = playerStats[0];
-                if (playerStatOld.score <= playerStatNew.score) {
-                    PlayerStat.findByIdAndRemove(playerStatOld.id).then(() => {
-                        createPlayerAndUpdateRanks(playerStatNew, res, currentMatch);
-                    },
-                        (err) => res.status(500).send(err.message + " : There was a problem finding the player stats."));
-                } else
-                    res.status(200).send(playerStats);
-            }
-        });
+    UserService.createStat(req.body.match, req.body.player, req.body.kills, req.body.score)
+        .then(playerStats => res.status(200).send(playerStats))
+        .catch(err => res.status(500).send(err.message + " : There was a problem finding the player stats."));
 
-    });
-    //},
-    // err => res.status(500).send("There was a problem finding the user."))
 });
-
-const createPlayerAndUpdateRanks = function (playerStatNew, res, currentMatch) {
-    PlayerStat.create(playerStatNew,
-        (err, playerStat) => {
-            if (err) return res.status(500).send(err.message + " : There was a problem adding the information to the database.");
-            PlayerStat.find({ match: currentMatch }, (err, playerStats) => {
-                if (err) return res.status(500).send(err.message + " : There was a problem adding the information to the database.");
-                let i = 0;
-                playerStats.sort((item1, item2) => {
-                    return item2.score - item1.score;
-                }).forEach(item => {
-                    item.rank = ++i;
-                    item.save();
-                })
-                res.status(200).send(playerStats);
-            });
-        });
-}
 
 // RETURNS ALL STATS  IN THE DATABASE FOR A PLAYER
 router.get('/playerStats/:id', function (req, res) {
-    PlayerStat.find({ player: ObjectId(req.params.id) }).exec().then((
+    UserService.getPlayerStats(req.params.id).then((
         playersStats) => res.status(200).send(playersStats))
         .catch(err => res.status(500).send(err.message + " : There was a problem finding the playersStats."));
 });
@@ -91,11 +42,9 @@ router.get('/playerStats/:id', function (req, res) {
 
 // RETURNS ALL THE PLAYERS IN THE DATABASE
 router.get('/players', function (req, res) {
+    UserService.getAllPlayers().then(players => res.status(200).send(players))
+        .catch(err => res.status(500).send("There was a problem finding the players."));
 
-    Player.find({}, function (err, players) {
-        if (err) return res.status(500).send("There was a problem finding the players.");
-        res.status(200).send(players);
-    });
 });
 
 // RETURNS ALL THE MATCHES IN THE DATABASE
@@ -109,18 +58,9 @@ router.get('/matches', function (req, res) {
 
 // RETURNS THE LEADERBOARD FOR MATCH AND TIME LIMIT
 router.get('/LeaderBoard', function (req, res) {
-    const timeInMillis = req.query.timeInMillis;
-    Match.find({ matchName: req.query.matchName }, function (err, matches) {
-        if (err) return res.status(500).send("There was a problem finding the matches.");
-        if (matches.length === 0) return res.status(404).send("Invalid match name provided.");
-        PlayerStat.find({ match: matches[0] }, function (err, playerStats) {
-            if (err) return res.status(500).send("There was a problem finding the stats.");
-            const result = playerStats.filter(item => item.statTime > timeInMillis).sort((item1, item2) => item2.score - item1.score).slice(0, 99)
-            res.status(200).send(result);
-        });
-    });
-
-
+    UserService.getMatchLeaderboard(req.query.matchName, req.query.timeInMillis)
+        .then(playerStats => res.status(200).send(playerStats))
+        .catch(err => res.status(500).send(err.message + " There was a problem finding the Leaderboard."));;
 });
 
 // RETURNS THE LEADERBOARD FOR MATCH AND TIME LIMIT
@@ -142,30 +82,7 @@ router.get('/LeaderBoard/:id', function (req, res) {
 });
 
 
-// GETS A SINGLE USER FROM THE DATABASE
-router.get('/:id', function (req, res) {
-    User.findById(req.params.id, function (err, user) {
-        if (err) return res.status(500).send("There was a problem finding the user.");
-        if (!user) return res.status(404).send("No user found.");
-        res.status(200).send(user);
-    });
-});
 
-// DELETES A USER FROM THE DATABASE
-router.delete('/:id', function (req, res) {
-    User.findByIdAndRemove(req.params.id, function (err, user) {
-        if (err) return res.status(500).send("There was a problem deleting the user.");
-        res.status(200).send("User: " + user.name + " was deleted.");
-    });
-});
-
-// UPDATES A SINGLE USER IN THE DATABASE
-router.put('/:id', function (req, res) {
-    User.findByIdAndUpdate(req.params.id, req.body, { new: true }, function (err, user) {
-        if (err) return res.status(500).send("There was a problem updating the user.");
-        res.status(200).send(user);
-    });
-});
 
 
 module.exports = router;
---
+++ user/UserService.js
@@ -0,0 +1,87 @@
+const mongoose = require('mongoose');
+const ObjectId = mongoose.Types.ObjectId;
+const Player = require('./models/Player');
+const Match = require('./models/Match');
+const PlayerStat = require('./models/PlayerStat');
+
+class UserService {
+    // CREATES A NEW PLAYER
+    createPlayer(playerName) {
+        return Player.create({ playerName });
+    }
+    // CREATES A NEW MATCH
+    createMatch(matchName) {
+        return Match.create({ matchName });
+    }
+    // CREATES A NEW STAT
+    createStat(matchName, playerId, kills, score) {
+        let currentMatch;
+        return Match.find({ matchName }).exec().then(matches => {
+            // if (err) return res.status(500).send(err.message + " : There was a problem finding the match.");
+            if (matches.length === 0) return res.status(404).send("Invalid match name provided.");
+            currentMatch = matches[0];
+            return PlayerStat.find({
+                player: ObjectId(playerId),
+                match: currentMatch
+            }).exec()
+        }).then(playerStats => {
+            const playerStatNew = {
+                player: ObjectId(playerId),
+                match: currentMatch,
+                kills: kills,
+                score: score,
+                statTime: new Date().getTime()
+            };
+            // if (err) return res.status(500).send(err.message + " : There was a problem finding the player stats.");
+            if (playerStats.length === 0) {
+                return this.createPlayerAndUpdateRanks(playerStatNew, currentMatch);
+            } else {
+                const playerStatOld = playerStats[0];
+                if (playerStatOld.score <= playerStatNew.score) {
+                    return PlayerStat.findByIdAndRemove(playerStatOld.id).exec().then(() => {
+                        return this.createPlayerAndUpdateRanks(playerStatNew, currentMatch);
+                    });
+                } else
+                    return playerStatOld;
+            }
+        });
+    }
+
+    createPlayerAndUpdateRanks(playerStatNew, currentMatch) {
+        return PlayerStat.create(playerStatNew).then(() => {
+            return PlayerStat.find({ match: currentMatch }).then(playerStats => {
+                let i = 0;
+                playerStats.sort((item1, item2) => {
+                    return item2.score - item1.score;
+                }).forEach(item => {
+                    item.rank = ++i;
+                    item.save();
+                })
+                return playerStats;
+            });
+        });
+    }
+
+    // RETURNS ALL STATS  IN THE DATABASE FOR A PLAYER
+    getPlayerStats(playerId) {
+        return PlayerStat.find({ player: ObjectId(playerId) }).exec();
+    }
+
+    // RETURNS ALL THE PLAYERS IN THE DATABASE
+    getAllPlayers() {
+        return Player.find({}).exec();
+    }
+
+    // RETURNS THE LEADERBOARD FOR MATCH AND TIME LIMIT
+    getMatchLeaderboard(matchName, timeInMillis) {
+        return Match.find({ matchName }).exec().then(matches => {
+            if (matches.length === 0) throw new Error("Invalid match name provided.");
+            return PlayerStat.find({ match: matches[0] }).exec().then(playerStats => {
+                return playerStats.filter(item => item.statTime > timeInMillis).sort((item1, item2) => item2.score - item1.score).slice(0, 99)
+            })
+        });
+    }
+}
+const instance = new UserService();
+
+module.exports = instance;
